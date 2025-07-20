d07299509e9637b8fd8722672a6b90d428e11c93
syntaxpadrino <edwinedu@proton.me>
2025-07-20 14:31:11 +0300

    Django Views and URL Configuration

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Library
+
+def list_books(request):
+
+    books = Book.objects.all()
+
+    book_list = []
+    for book in books:
+        book_list.append(f"{book.title} by {book.author.name}")
+
+    books_text = "\n".join(book_list) if book_list else "No books available in the database."
+
+    return HttpResponse(books_text, content_type = "text/plain")
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/list_books.html'
+    context_object_name = 'library'
+
+    def get_context_date(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        library = self.get_object()
+        context['books'] = library.books.all()
+
+        return context
