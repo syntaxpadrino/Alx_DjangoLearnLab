fd8021b7a5d18a7267d3a05e4c0dff59171e4831
syntaxpadrino <edwinedu@proton.me>
2025-07-20 14:35:21 +0300

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book
-from .models import Library
-
+from .models import Book, Library
 
 def list_books(request):
 
@@ -18,7 +16,7 @@ def list_books(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'relationship_app/list_books.html', 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_context_date(self, **kwargs):
