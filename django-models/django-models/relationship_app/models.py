6d71ce57f4cb19967576de75a9698b38c9aea11a
syntaxpadrino <edwinedu@proton.me>
2025-07-20 12:47:10 +0300

    My alx checker error check

---
+++ django-models/django-models/relationship_app/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+# git status
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+    
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+    
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
