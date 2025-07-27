6f06ff670466a134c90e156340b70cf48171787c
syntaxpadrino <edwinedu@proton.me>
2025-07-27 18:52:07 +0300

    Implement user auth in django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-from .views import list_books
 
 urlpatterns = [
     # Function based view - Lists all books
-    # Urlpattern - /books/
-    # view
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    # Authentication URLs
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
 ]
--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -128,3 +128,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/books/'  # Redirect after login
