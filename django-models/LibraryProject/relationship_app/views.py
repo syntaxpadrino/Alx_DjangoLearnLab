b6d74c13c7f67f187364e8f0b5ceecb6920fc35f
syntaxpadrino <edwinedu@proton.me>
2025-07-27 19:12:07 +0300

    Implement user auth in django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -37,7 +37,7 @@ def register(request):
     else:
         form = UserCreationForm()
     
-    return render(request, 'relationship_app/templates/registration/register.html', {'form': form})
+    return render(request, 'relationship_app/register.html', {'form': form})
 
 @login_required
 def profile(request):
